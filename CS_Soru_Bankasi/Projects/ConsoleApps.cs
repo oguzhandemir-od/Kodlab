54245ccccd8119ee0132a2143236a34969b776ee
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-08-29 22:50:10 +0300

    Question 3

--- CS_Soru_Bankasi/Projects/ConsoleApps.cs
+++ CS_Soru_Bankasi/Projects/ConsoleApps.cs
@@ -12,5 +12,13 @@ class ConsoleApps
     {
         Console.WriteLine("Merhaba, C# Kullanıcıları");
     }
+
+    // Using Write method, write this output on screen: A=50B=25
+    public static void Question3()
+    {
+        int a=50,b=25;
+        Console.Write("A={0}",a);
+        Console.Write("B={0}",b);
+    }
     #endregion
 }
