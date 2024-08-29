db1f5b88324a52f8e501e9b329d71cb2556f4934
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-08-29 22:48:11 +0300

    Question 1

---
+++ CS_Soru_Bankasi/Projects/ConsoleApps.cs
@@ -0,0 +1,10 @@
+class ConsoleApps
+{
+    #region Write and WriteLine Methods
+    // Using Write method, write "Merhaba, C# Kullanıcıları" on screen
+    public static void Question1()
+    {
+        Console.Write("Merhaba, C# Kullanıcıları");
+    }
+    #endregion
+}
