6b905cb116b63b7713c879d8cb4e9c6f3b2da20e
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-08-29 22:57:16 +0300

    Question 9

--- CS_Soru_Bankasi/Projects/ConsoleApps.cs
+++ CS_Soru_Bankasi/Projects/ConsoleApps.cs
@@ -82,4 +82,21 @@ class ConsoleApps
         Console.WriteLine("{0} tuşuna bastınız",pushKey.Key.ToString());
     }
     #endregion
+
+    #region SetCursorPosition Method
+    // Using SetCursorPosition method, write the codes that type 100 (*) on screen at random points
+    public static void Question9()
+    {
+        Random rnd=new Random();
+        Console.SetWindowSize(30,20);
+        for (int i = 0; i < 100; i++)
+        {
+            int width=rnd.Next(1,29);
+            int height=rnd.Next(1,19);
+            Console.SetCursorPosition(width,height);
+            Console.WriteLine("*");
+            Thread.Sleep(150);
+        }
+    }
+    #endregion
 }
