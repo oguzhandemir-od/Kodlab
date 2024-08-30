abfaec54da2b0d0656ae024f8336a235aaae0aa2
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-08-30 17:27:37 +0300

    Question 2

--- CS_Soru_Bankasi/Projects/ConstsAndVars.cs
+++ CS_Soru_Bankasi/Projects/ConstsAndVars.cs
@@ -15,4 +15,30 @@ class ConstsAndVars
         Console.Write("Toplam tutar: {0} TL",total);
     }
     #endregion
+
+    #region Variable Types
+    // Assign values to Short, Sbyte, Int, Decimal, String, Double, Char, Bool and Float variable types
+    // Write these values on screen top and bottom
+    public static void Question2()
+    {
+        short shortVar=125;
+        sbyte sbyteVar=-25;
+        int integerVar=54786;
+        decimal decimalVar=123.45m;
+        string stringVar="Kitap";
+        double doubleVar=456678;
+        char charVar='&';
+        bool booleanVar=true;
+        float floatVar=-2541.78f;
+        Console.WriteLine("Short {0}",shortVar);
+        Console.WriteLine("SByte {0}",sbyteVar);
+        Console.WriteLine("Integer {0}",integerVar);
+        Console.WriteLine("Decimal {0}",decimalVar);
+        Console.WriteLine("String {0}",stringVar);
+        Console.WriteLine("Double {0}",doubleVar);
+        Console.WriteLine("Char {0}",charVar);
+        Console.WriteLine("Boolean {0}",booleanVar);
+        Console.WriteLine("Float {0}",floatVar);
+    }
+    #endregion
 }
